8ea2de53459dd753fc39cc84f0a970688b4095ae
antonJagupov <114066156+antonJagupov@users.noreply.github.com>
2026-05-07 22:54:50 +0300

    Update App.tsx

--- task 2/frontend/src/App.tsx
+++ task 2/frontend/src/App.tsx
@@ -6,15 +6,22 @@ import ReportPage from './components/ReportPage';
 const keycloakConfig: KeycloakConfig = {
   url: process.env.REACT_APP_KEYCLOAK_URL,
   realm: process.env.REACT_APP_KEYCLOAK_REALM||"",
-  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID||"",
-  pkceMethod: 'S256'
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID||""
 };
 
 const keycloak = new Keycloak(keycloakConfig);
 
+const initOptions = {
+  onLoad: 'check-sso',
+  pkceMethod: 'S256'   
+};
+
 const App: React.FC = () => {
   return (
-    <ReactKeycloakProvider authClient={keycloak}>
+    <ReactKeycloakProvider 
+		  authClient={keycloak}
+		  initOptions={initOptions} 	
+	  >
       <div className="App">
         <ReportPage />
       </div>
